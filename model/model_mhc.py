09a3de579e214c0a0fa65d2f18126eb574d2f58d
phoenix <pengxiangzhao@connect.hku.hk>
2026-07-09 14:38:47 +0800

    add comb overall scale

--- model/model_mhc.py
+++ model/model_mhc.py
@@ -179,6 +179,7 @@ class HyperConnection(nn.Module):
         # finite-iteration row/column mass drift.
         # comb = comb / (comb.sum(dim=-1, keepdim=True) + self.hc_eps)
         # comb = comb / (comb.sum(dim=-2, keepdim=True) + self.hc_eps)
+        comb *= 1.1
         return comb
 
     def compute_mix(self, hidden_streams: torch.Tensor) -> torch.Tensor:
